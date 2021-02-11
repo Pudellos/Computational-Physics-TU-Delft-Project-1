e7cffd7e7300f9b0f0e1a0ea00e43a2f33779ffa
Klara Chmiel <k.m.chmiel@student.tudelft.nl>
2021-02-11 11:22:10 +0100

    changing functions to match skeleton given, init volume of gas added to gas class

--- main_module.py
+++ main_module.py
@@ -10,8 +10,9 @@ class Molecule:
         
 class Gas:
     '''Class Gas, describes collection of instances of Class Molecule'''
-    def __init__(self, molecules):
+    def __init__(self, molecules, volume):
         self.molecules = molecules
+        self.volume = volume
     
     def positions(self):
         '''Returns positions of all gas molecules'''
@@ -31,17 +32,24 @@ class Gas:
             couples.append(couple)
         return(couples)
     
-    def LJ_potentials(self, differenciate_gas=False, symbol_mode_gas=False, P_gas=10*5, V_gas=1, T_gas=273.15):
+    def lj_potentials(self, differenciate_gas=False, symbol_mode_gas=False, P_gas=10*5, V_gas=True, T_gas=273.15):
         '''Returns Lennard Johnes potentials values corresponding to couples of molecules from output of couples_of_molecules function
         
         use help(main.LJ.LJ_potential) for details about parameters'''
         
         potentials=[]
         for i in self.couples_of_molecules():
-            potentials.append(LJ.LJ_potential(i,len(self.molecules),P=P_gas, V=V_gas, T=T_gas, symbol_mode=symbol_mode_gas, differenciate=differenciate_gas))
+            potentials.append(LJ.LJ_potential(i,len(self.molecules),P=P_gas, V=self.volume, T=T_gas, symbol_mode=symbol_mode_gas, differenciate=differenciate_gas))
         return(potentials)
     
-    def force_due_to_LJ(self,chosen,P=10*5,V=1,T=273.15):
+    def atomic_distances(self):
+        '''returns distances between all iterated pairs of molecules in the simulation corresponding to pairs printed by couples_of_molecules() function'''
+        distances=[]
+        for i in range(len(self.couples_of_molecules())):
+            distances.append(abs(self.couples_of_molecules()[i][0]-self.couples_of_molecules()[i][1]))
+        return(distances)
+    
+    def lj_force(self,chosen,P=10*5,V=True,T=273.15):
         '''Returns forces on a particle due to Lennard Johnes potential corresponding to couples of molecules from output of couples_of_molecules function
         fromula:  force = - d(potential)/dr * grad (distance between two molecules)
                 where grad (distance between two molecules) = abs(r_1 - r_2)
@@ -52,20 +60,14 @@ class Gas:
         force=0
         index=0
         for i in self.couples_of_molecules():
-            temp_potentials=self.LJ_potentials(differenciate_gas=True, symbol_mode_gas=False, P_gas=P, V_gas=V, T_gas=T)
+            temp_potentials=self.lj_potentials(differenciate_gas=True, symbol_mode_gas=False, P_gas=P, V_gas=self.volume, T_gas=T)
         for i in self.couples_of_molecules():
             for j in i:
                 if j==chosen:
-                    force+=-1*temp_potentials[index]*abs(self.couples_of_molecules()[index][0]-self.couples_of_molecules()[index][1])
+                    force+=-1*temp_potentials[index]*self.atomic_distances()[index]
             index+=1
         return(force)
     
-    def atomic_distances(self):
-        '''returns distances between all iterated pairs of molecules in the simulation corresponding to pairs printed by couples_of_molecules() function'''
-        distances=[]
-        for i in range(len(self.couples_of_molecules())):
-            distances.append(abs(self.couples_of_molecules()[i][0]-self.couples_of_molecules()[i][1]))
-        return(distances)
 
     
 #EXAMPLE OF CREATING CLASS INSTANCES AND USING FUNCTIONS
@@ -73,6 +75,6 @@ class Gas:
 # g=Gas(atoms)
 # print(g.positions())
 # print(g.couples_of_molecules())
-# print(g.LJ_potentials())
-# print(g.force_due_to_LJ(1))
-# print(g.force_due_to_LJ(2))
+# print(g.lj_potentials())
+# print(g.lj_force(1))
+# print(g.lj_force(2))
--- simulation.py
+++ simulation.py
@@ -6,7 +6,7 @@ from main_module import Molecule
 #specify separate molecules in the simulation:
 atoms = np.array((Molecule(1,2),Molecule(2,4),Molecule(3,5),Molecule(5,6)))
 #combine them into a gas
-g=Gas(atoms)
+g=Gas(atoms,1)
 
 #examples of class methods for various quantities:
 print('positions of the gas molecules are:')
@@ -22,22 +22,19 @@ print(g.couples_of_molecules())
 print()
 
 print('Lennard Johnes potentials between those pairs:')
-print(g.LJ_potentials())
+print(g.lj_potentials())
 print()
 
 print('derivative of Lennard Johnes potentials between those pairs:')
-print(g.LJ_potentials(True))
+print(g.lj_potentials(True))
 print()
 
 print('force on a chosen molecule in the gas due to lennard johnes potentials:')
 print('example: force on molecule with position = 1 due to all other molecules:')
-print(g.force_due_to_LJ(1))
+print(g.lj_force(1))
 print('example: force on molecule with position = 2 due to all other molecules:')
-print(g.force_due_to_LJ(2))
-
-print('distances between pairs of molecules in the simulation as given by couples_of_molecules():')
-print(g.atomic_distances())
+print(g.lj_force(2))
 
 print()
 print('see help(function) for detailed describtion of all parameters, how to change temperature, pressure etc of the simulation')
-# print(help(g.force_due_to_LJ))
+# print(help(g.lj_force))
